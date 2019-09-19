03c6dbff96da1b2c11a1986775174644c67f0970
dfischer17 <dfischer17htl@outlook.com>
2019-09-19 21:09:24 +0200

    Konstuktor hinzugefuegt

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -2,11 +2,25 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @Override
     public boolean isPrime(int p) {
-        return false;
+        if (p < 2) {
+            return false;
+        }
+        else {
+            for (int i = 2; i < p; i++) {
+                if (p % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
     public void printPrimes() {
 
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
