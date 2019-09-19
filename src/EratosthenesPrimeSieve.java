9b1908234272ae94db1e719079bb0c3256f7632e
dfischer17 <dfischer17htl@outlook.com>
2019-09-19 22:00:20 +0200

    Algorythmus implemetiert

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,4 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
+    private int p;
+
+    public EratosthenesPrimeSieve(int p) {
+        this.p = p;
+    }
 
     @Override
     public boolean isPrime(int p) {
@@ -17,10 +25,32 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @Override
     public void printPrimes() {
+        for (int k = 0; k < p; k++) {
+            if (isPrime(k)) {
+                System.out.println(k);
+            }
+        }
+    }
 
+    public void algorithm() {
+        // Liste der Potenziellen Primzahlen erstellen
+        Map<Integer, Boolean> primeNumbersList = new HashMap<>();
+        for (int i = 1; i <= p; i++) {
+            primeNumbersList.put(i, false);
+        }
+
+        //wenn Primzahl gefunden ==> alle Vielfache markieren
+        for (int i = 1; i <= p; i++) {
+            if (isPrime(i)) {
+                for (int k = 1; k < p; k++) {
+                    primeNumbersList.put(i*k, true);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        
+        EratosthenesPrimeSieve e = new EratosthenesPrimeSieve(10);
+        e.algorithm();
     }
 }
