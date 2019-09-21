dba145c4b09d235d2d36d51fc8ebe5cd17136fef
dfischer17 <dfischer17htl@outlook.com>
2019-09-21 12:59:49 +0200

    Aufgabe2 Initialer commit

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EratosthenesPrimeSieve implements PrimeSieve{
@@ -32,6 +34,7 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
         }
     }
 
+    // Aufgabe1
     public void algorithm() {
         boolean[] arr = new boolean[p + 1];
 
@@ -45,8 +48,37 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
                 }
             }
         }
+    }
 
+    // Aufgabe2
+    public void algorithm2() {
+        /*
+        Wir wissen, N sind alle natürlichen Zahlen. Wir denieren nun die Menge Ng als die Menge aller
+        geraden natürlichen Zahlen. Weiters sei P die Menge aller Primzahlen. Es gibt nun eine Vermutung
+        die besagt: Für jede Zahl n ∈ Ng, wobei n > 2 gilt, gibt es zwei Zahlen x, y ∈ P sodass gilt: n = x+y.
+        Beispiele sind: 5 + 13 = 18, 2 + 2 = 4, 3 + 3 = 6
+        Man kann leicht einsehen, dass die Summe nicht eindeutig sein muss: 7 + 11 = 5 + 13 = 18.
+        */
 
+        /*
+        Implementieren Sie einen Algorithmus, welcher für jede gerade natürliche Zahl n > 2 bis zu einer
+        Obergrenze o ∈ N zwei Primzahlen x, y ∈ P ndet, sodass gilt: x + y = n. Verwenden Sie dazu die
+        Klasse EratosthenesPrimeSieve
+        */
+        List<Integer> primeNumbers = new ArrayList<>();
+        // zwei Primzahlen addieren welche eine gerade Zahl < 2 ergeben
+
+            // Alle Primzahlen bis Obergrenze herausfinden
+            for (int i = 2; i <= p; i++) {
+                if (isPrime(i)) {
+                    primeNumbers.add(i);
+                }
+            }
+
+            // x + y Werte herausfinden
+            for (int i = 0; i < ; i++) {
+                
+            }
     }
 
     public static void main(String[] args) {
